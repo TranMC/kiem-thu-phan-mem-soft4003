bbc2bd35a9026aab41196c3cd72ce8c2bc0c883f
TranMC <tranmc2005@gmail.com>
2026-01-07 17:30:31 +0700

    Upload basic code

---
+++ unit-test/src/java/StudentUtils.java
@@ -0,0 +1,11 @@
+package java;
+public class StudentUtils {
+
+    public static int countExcellentStudents(double[] scores) {
+        return 0;
+    }
+
+    public static double calculateValidAverage(double[] scores) {
+        return 0;
+    }
+}
---
+++ unit-test/test/java/StudentUtilsTest.java
@@ -0,0 +1,16 @@
+package test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentUtilsTest {
+
+    @Test
+    void testCountExcellentStudents_validInputs() {
+        // TODO
+    }
+
+    @Test
+    void testCalculateValidAverage_mixedValidInvalid() {
+        // TODO
+    }
+}
