bb9e08375910f8495971d4c47d0157093a7ec72d
TranMC <tranmc2005@gmail.com>
2026-01-13 17:17:30 +0700

    Update config

--- cypress-exercise/cypress.config.js
+++ cypress-exercise/cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    screenshotOnRunFailure: true,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
